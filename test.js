4887f2f24e0007ca50051ac9446f04b793fc77a9
ANarngAung <anarngaung2-17@gmail.com>
2024-06-01 12:49:25 +0630

    repiar html file

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
-document.querySelectorAll('.monk-card button').forEach(button => {
-    button.addEventListener('click', () => {
-        alert('You clicked "Reach Out To Me"');
+function showContent(id) {
+    // Hide all content elements
+    var contents = document.querySelectorAll('.content');
+    contents.forEach(function(content) {
+        content.classList.remove('active');
     });
-});
+
+    // Show the clicked content element
+    var contentToShow = document.getElementById(id);
+    contentToShow.classList.add('active');
+}
