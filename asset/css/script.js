6c6fa47b06e9fb255902aa49180b7ad24ef2bb01
ANarngAung <anarngaung2-17@gmail.com>
2024-06-02 23:53:10 +0630

    repiar css files

--- asset/css/script.js
+++ asset/css/script.js
@@ -1,16 +1,16 @@
-// const track = document.querySelector('.carousel-track');
-//     let index = 0;
+const track = document.querySelector('.carousel-track');
+    let index = 0;
 
-//     function moveCarousel() {
-//         index++;
-//         if (index >= track.children.length) {
-//             index = 0;
-//         }
-//         const translateX = -index * (track.children[0].offsetWidth + 20); // 20 is the margin value
-//         track.style.transform = `translateX(${translateX}px)`;
-//     }
+    function moveCarousel() {
+        index++;
+        if (index >= track.children.length) {
+            index = 0;
+        }
+        const translateX = -index * (track.children[0].offsetWidth + 20); // 20 is the margin value
+        track.style.transform = `translateX(${translateX}px)`;
+    }
 
-//     setInterval(moveCarousel, 3000);
+    // setInterval(moveCarousel, 3000);
 
 // function showContent(id) {
 //     // Hide all content elements
