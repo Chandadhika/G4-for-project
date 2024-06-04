174a29a578016cfd831d25c0bf0f73f648a949c9
ANarngAung <anarngaung2-17@gmail.com>
2024-06-05 03:17:05 +0630

    replace bg-photo and repiar font-size

--- asset/css/script.js
+++ asset/css/script.js
@@ -1,25 +1,25 @@
-const track = document.querySelector('.carousel-track');
-    let index = 0;
+// const track = document.querySelector('.carousel-track');
+//     let index = 0;
 
-    function moveCarousel() {
-        index++;
-        if (index >= track.children.length) {
-            index = 0;
-        }
-        const translateX = -index * (track.children[0].offsetWidth + 20); // 20 is the margin value
-        track.style.transform = `translateX(${translateX}px)`;
-    }
+//     function moveCarousel() {
+//         index++;
+//         if (index >= track.children.length) {
+//             index = 0;
+//         }
+//         const translateX = -index * (track.children[0].offsetWidth + 20); // 20 is the margin value
+//         track.style.transform = `translateX(${translateX}px)`;
+//     }
 
-    // setInterval(moveCarousel, 3000);
+//     // setInterval(moveCarousel, 3000);
 
-// function showContent(id) {
-//     // Hide all content elements
-//     var contents = document.querySelectorAll('.content');
-//     contents.forEach(function(content) {
-//         content.classList.remove('active');
-//     });
+function showContent(id) {
+    // Hide all content elements
+    var contents = document.querySelectorAll('.content');
+    contents.forEach(function(content) {
+        content.classList.remove('active');
+    });
 
-//     // Show the clicked content element
-//     var contentToShow = document.getElementById(id);
-//     contentToShow.classList.add('active');
-// }
+    // Show the clicked content element
+    var contentToShow = document.getElementById(id);
+    contentToShow.classList.add('active');
+}
