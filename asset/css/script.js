b779e0bfb3f4320893d76b6dad088fad11d47ade
ANarngAung <anarngaung2-17@gmail.com>
2024-06-05 07:54:21 +0630

    added javascript styles

--- asset/css/script.js
+++ asset/css/script.js
@@ -1,16 +1,3 @@
-// const track = document.querySelector('.carousel-track');
-//     let index = 0;
-
-//     function moveCarousel() {
-//         index++;
-//         if (index >= track.children.length) {
-//             index = 0;
-//         }
-//         const translateX = -index * (track.children[0].offsetWidth + 20); // 20 is the margin value
-//         track.style.transform = `translateX(${translateX}px)`;
-//     }
-
-//     // setInterval(moveCarousel, 3000);
 
 function showContent(id) {
     // Hide all content elements
@@ -22,4 +9,8 @@ function showContent(id) {
     // Show the clicked content element
     var contentToShow = document.getElementById(id);
     contentToShow.classList.add('active');
+
+    // var contents = document.querySelectorAll('.content');
+    // contents.forEach(function(content) {
+    //     content.classList.remove('active');
 }
